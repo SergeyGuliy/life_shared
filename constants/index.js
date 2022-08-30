75e0d4db6a9d5b47ad5f425d06753353de58d3cc
sergey <sergey.guliy.92@gmail.com>
2022-08-31 01:03:10 +0300

    change setting to use module.exports

--- constants/index.js
+++ constants/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable */
-
-export const LOGOUT_TIMEOUT = 5000;
-
-export const SALT_ROUND_CRYPT = 2;
-
-export const UPDATE_TIME_DELTA = 10000;
+module.exports = {
+  LOGOUT_TIMEOUT: 5000,
+  SALT_ROUND_CRYPT: 2,
+  UPDATE_TIME_DELTA: 10000
+}
