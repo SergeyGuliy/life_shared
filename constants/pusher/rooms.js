581f8f98f8d73cfa4d25fb4dbc4b9b370d34aca2
sergey <sergey.guliy.92@gmail.com>
2022-09-02 04:35:59 +0300

    move rooms events in constants

---
+++ constants/pusher/rooms.js
@@ -0,0 +1,33 @@
+const rooms_userConnectsRoom = 'rooms_userConnectsRoom'
+const rooms_subscribeRoomsUpdate = 'rooms_subscribeRoomsUpdate'
+const rooms_unSubscribeRoomsUpdate = 'rooms_unSubscribeRoomsUpdate'
+const rooms_kickUserFromRoom = 'rooms_kickUserFromRoom'
+const rooms_setNewAdminInRoom = 'rooms_setNewAdminInRoom'
+const rooms_updateUsersListInRoom = 'rooms_updateUsersListInRoom'
+const rooms_updateRoomAdmin = 'rooms_updateRoomAdmin'
+const rooms_userLeaveRoom = 'rooms_userLeaveRoom'
+const rooms_userKickedFromRoom = 'rooms_userKickedFromRoom'
+const rooms_updateToggleLockRoom = 'rooms_updateToggleLockRoom'
+const rooms_roomInListCreated = 'rooms_roomInListCreated'
+const rooms_roomInListDeleted = 'rooms_roomInListDeleted'
+const rooms_roomInListUpdated = 'rooms_roomInListUpdated'
+const rooms_userJoinRoom = 'rooms_userJoinRoom'
+const rooms_RoomsUpdater = 'rooms_RoomsUpdater'
+
+module.exports = {
+  rooms_userConnectsRoom,
+  rooms_subscribeRoomsUpdate,
+  rooms_unSubscribeRoomsUpdate,
+  rooms_kickUserFromRoom,
+  rooms_setNewAdminInRoom,
+  rooms_updateUsersListInRoom,
+  rooms_updateRoomAdmin,
+  rooms_userLeaveRoom,
+  rooms_userKickedFromRoom,
+  rooms_updateToggleLockRoom,
+  rooms_roomInListCreated,
+  rooms_roomInListDeleted,
+  rooms_roomInListUpdated,
+  rooms_userJoinRoom,
+  rooms_RoomsUpdater
+}
