5b92ca30096e9047ad334214972fc4cdf6c1371d
sergey <sergey.guliy.92@gmail.com>
2022-09-02 04:50:05 +0300

    move socketSetup constants into library

---
+++ constants/ws/socketSetup.js
@@ -0,0 +1,9 @@
+const socketSetup_giveUserIdToServer = 'socketSetup_giveUserIdToServer'
+const socketSetup_callUserIdToServer = 'socketSetup_callUserIdToServer'
+const socketSetup_forceDisconnect = 'socketSetup_forceDisconnect'
+
+module.exports = {
+  socketSetup_giveUserIdToServer,
+  socketSetup_callUserIdToServer,
+  socketSetup_forceDisconnect
+}
