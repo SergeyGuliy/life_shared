088e65b0f511ea9180999472c2626e80e3918611
sergey <sergey.guliy.92@gmail.com>
2022-09-03 00:59:38 +0300

    move enums into library

--- enums/index.js
+++ enums/index.js
@@ -19,6 +19,12 @@ const MESSAGE_RECEIVER_TYPES = {
   PRIVATE: 'PRIVATE',
 }
 
+const MESSAGES_TYPES_MAP = {
+  GLOBAL: "global",
+  ROOM: "room",
+  PRIVATE: "private"
+};
+
 const FRIENDSHIP_STATUSES = {
   PENDING: 'PENDING',
   APPROVED: 'APPROVED',
@@ -41,6 +47,11 @@ const LOCALES = {
   ru: 'ru',
 }
 
+const LOCALES_WITH_KEYS = [
+  { title: "English", key: LOCALES.en },
+  { title: "Russian", key: LOCALES.ru }
+];
+
 const SOUNDS = {
   SOUNDS_1: 'SOUNDS_1',
   SOUNDS_2: 'SOUNDS_2',
@@ -49,14 +60,19 @@ const SOUNDS = {
   SOUNDS_5: 'SOUNDS_5',
 }
 
+const COUNTRIES = ["England", "Russia", "Ukraine", "Hungary", "Italy"];
+
 module.exports = {
   USER_ROLES,
   ROOM_TYPES,
   MESSAGE_TYPES,
   MESSAGE_RECEIVER_TYPES,
+  MESSAGES_TYPES_MAP,
   FRIENDSHIP_STATUSES,
   USER_ONLINE_STATUSES,
   USER_GAME_STATUSES,
   LOCALES,
+  LOCALES_WITH_KEYS,
   SOUNDS,
+  COUNTRIES
 }
